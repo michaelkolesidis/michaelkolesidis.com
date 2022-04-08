ba8c3e42624b0d593b0cd08b63194434c6c15fcd
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-04-08 14:58:10 +0300

    added circle

---
+++ scripts/sketch.js
@@ -0,0 +1,23 @@
+let x, y;
+let newX, newY;
+
+function setup() {
+  let cnv = createCanvas(400, 400);
+  cnv.parent("sketch");
+  background(255);
+  x = width / 2;
+  y = height / 2;
+}
+
+function draw() {
+  strokeWeight(0.1);
+
+  let radius = random(200);
+  let a = random(TWO_PI);
+
+  newX = width / 2 + cos(a) * radius;
+  newY = height / 2 + sin(a) * radius;
+  line(x, y, newX, newY);
+  x = newX;
+  y = newY;
+}
