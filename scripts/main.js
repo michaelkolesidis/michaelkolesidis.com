7c9c4a262ff53b391acd2c5d993a34d7f14b7045
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-04-20 00:03:54 +0300

    added project and technologies

--- scripts/main.js
+++ scripts/main.js
@@ -17,16 +17,17 @@ root.innerHTML = /*html*/ `
 <p>You can contact me at michael.kolesidis(at)gmail.com</p>
 <br>
 <h2>Projects</h2>
-<a href="https://javascript-software-synthesizer.netlify.app/" target="_blank">JavaScript Software Synthesizer</a><br>
-<a href="https://minesweeper-emoji.netlify.app/" target="_blank" rel="noopener noreferrer">Minesweeper Emoji</a><br>
-<a href="https://michaelkolesidis.github.io/project-japanese-restaurant/" target="_blank" rel="noopener noreferrer">Japanese Restaurant</a><br>
+<a href="https://javascript-software-synthesizer.netlify.app/" target="_blank">JavaScript Software Synthesizer | Tone.js</a><br>
+<a href="https://minesweeper-emoji.netlify.app/" target="_blank" rel="noopener noreferrer">Minesweeper Emoji | p5.js</a><br>
+<a href="https://react-landmarks.netlify.app/" target="_blank" rel="noopener noreferrer">Landmarks | React</a><br>
+<a href="https://michaelkolesidis.github.io/project-japanese-restaurant/" target="_blank" rel="noopener noreferrer">Japanese Restaurant | ES6 Modules</a><br>
 <a href="https://project-olympics.netlify.app/" target="_blank" rel="noopener noreferrer">Olympics™</a><br>
-<a href="https://sports-app-pwa.netlify.app/" target="_blank" rel="noopener noreferrer">Sports App (Progressive Web App)</a><br>
-<a href="https://openprocessing.org/sketch/1279127" target="_blank" rel="noopener noreferrer">3D Cube</a><br>
+<a href="https://sports-app-pwa.netlify.app/" target="_blank" rel="noopener noreferrer">Sports App | Progressive Web App</a><br>
+<a href="https://openprocessing.org/sketch/1279127" target="_blank" rel="noopener noreferrer">3D Cube | Processing</a><br>
 <a href="https://electronic-drumkit-js.netlify.app/" target="_blank" rel="noopener noreferrer">Electronic Drum Kit</a><br>
 <a href="https://css-3d.netlify.app/" target="_blank" rel="noopener noreferrer">CSS 3D</a><br>
-<a href="https://circle-random-lines.netlify.app/" target="_blank" rel="noopener noreferrer">Circle Random Lines</a><br>
-<a href="https://move-square.netlify.app/" target="_blank" rel="noopener noreferrer">Move Square</a>
+<a href="https://circle-random-lines.netlify.app/" target="_blank" rel="noopener noreferrer">Circle Random Lines | p5.js</a><br>
+<a href="https://move-square.netlify.app/" target="_blank" rel="noopener noreferrer">Move Square | SVG</a>
 <br>
 <br>
 <h2>Links</h2>
