0c204b11d729485fbaa6b09989dd6d59926ac4e5
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-04-20 21:50:23 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -20,10 +20,10 @@ root.innerHTML = /*html*/ `
 <a href="https://javascript-software-synthesizer.netlify.app/" target="_blank">JavaScript Software Synthesizer | Tone.js</a><br>
 <a href="https://minesweeper-emoji.netlify.app/" target="_blank" rel="noopener noreferrer">Minesweeper Emoji | p5.js</a><br>
 <a href="https://react-landmarks.netlify.app/" target="_blank" rel="noopener noreferrer">Landmarks | React</a><br>
-<a href="https://michaelkolesidis.github.io/project-japanese-restaurant/" target="_blank" rel="noopener noreferrer">Japanese Restaurant | ES6 Modules</a><br>
+<a href="https://michaelkolesidis.github.io/japanese-restaurant/" target="_blank" rel="noopener noreferrer">Japanese Restaurant | ES6 Modules</a><br>
 <a href="https://project-olympics.netlify.app/" target="_blank" rel="noopener noreferrer">Olympics™</a><br>
 <a href="https://sports-app-pwa.netlify.app/" target="_blank" rel="noopener noreferrer">Sports App | Progressive Web App</a><br>
-<a href="https://openprocessing.org/sketch/1279127" target="_blank" rel="noopener noreferrer">3D Cube | Processing</a><br>
+<a href="https://3d-cubejs.netlify.app/" target="_blank" rel="noopener noreferrer">3D Cube | p5.js</a><br>
 <a href="https://electronic-drumkit-js.netlify.app/" target="_blank" rel="noopener noreferrer">Electronic Drum Kit | JavaScript</a><br>
 <a href="https://css-3d.netlify.app/" target="_blank" rel="noopener noreferrer">CSS 3D</a><br>
 <a href="https://circle-random-lines.netlify.app/" target="_blank" rel="noopener noreferrer">Circle Random Lines | p5.js</a><br>
