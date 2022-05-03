927dce714d027c8c1aff1a2d40b848eefe840967
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-05-03 23:34:58 +0300

    main module

--- scripts/main.js
+++ scripts/main.js
@@ -51,3 +51,5 @@ root.innerHTML = /*html*/ `
 
 <p id="footnote">*Most projects include a link to their GitHub repository.<br>All projects have informative readme files.You can find<br>all relevant information about eact project there.</p>
 `;
+
+export let darkMode = false;
