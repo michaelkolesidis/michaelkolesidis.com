5b43c8fe187ca5e5a7b68674722d8a74c5158b06
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-06-09 01:07:02 +0300

    tech stack section

---
+++ scripts/components/Technologies.js
@@ -0,0 +1,20 @@
+// © 2022 Michael Kolesidis. All rights reserved.
+// michael.kolesidis@gmail.com
+
+export default function technologies() {
+  // Project Title
+  let technologiesTitle = document.createElement("div");
+  technologiesTitle.classList.add("wavy");
+  technologiesTitle.innerHTML = `   
+      <span style="--i:1">T</span>
+      <span style="--i:2">E</span>
+      <span style="--i:3">C</span>
+      <span style="--i:4">H</span>
+      <span style="--i:5"> </span>
+      <span style="--i:6">S</span>
+      <span style="--i:7">T</span>
+      <span style="--i:8">A</span>
+      <span style="--i:9">C</span>
+      <span style="--i:9">K</span>`;
+  projects.appendChild(technologiesTitle);
+}
