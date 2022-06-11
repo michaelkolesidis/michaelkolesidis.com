f5beec5553b1741f5f7d41f10aa75b5ad8910c3d
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-06-12 02:10:29 +0300

    about update

--- scripts/components/About.js
+++ scripts/components/About.js
@@ -23,12 +23,15 @@ export default function about() {
     "Haven't you seen/read enough already? No? OK, I'll tell you a bit more about me.",
     "I have lived, studied and worked in Greece (Thessaloniki), Spain (Barcelona) and the UK (Brighton).",
     "I speak Greek, English, Spanish, and a bit of (actually hardly any nowadays) German.",
-    "I love working with React, JavaScript, Three.js and Sass.",
+    "I love working with React and Sass.",
+    "The first programming language I ever \"tampered with\" was Pascal, and the first language I ever learnt was C.",
+    "C remains one of my favourite languages even though I hardly ever use it knowadays.",
     "I write almost all my code on VSCode.",
     "I use Linux (Ubuntu) as my daily drive, Windows 11 (for Adobe) and my good ol' MacBook when I'm not at home.",
     "I've been  to many countries.",
-    "I am currently learning functional programming with OCaml.",
     "I don't drink coffee or tea.",
+    "I enjoy all board sports.",
+    "The sport I'm best at is probably snowboarding.",
     "I hope that's enough for now!",
   ];
 
