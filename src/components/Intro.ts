52be71fabb7fa2ffc5d8c253da7a65c2577a4ccf
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-02-22 18:52:21 +0200

    into text

--- src/components/Intro.ts
+++ src/components/Intro.ts
@@ -9,7 +9,7 @@ export default function intro() {
     // Who Am I (paragraph on the left)
     const whoami = document.createElement("p");
     whoami.setAttribute("id", "left");
-    whoami.innerHTML = `I'm Michael Kolesidis.(as you might have already guessed from the huge heading)`;
+    whoami.innerHTML = `I'm Michael Kolesidis.<br>(as you might have already guessed from the huge heading)`;
     intro.appendChild(whoami);
 
     // Handwriting
@@ -26,7 +26,7 @@ export default function intro() {
     // What Am I (paragraph on the right)
     const whatami = document.createElement("p");
     whatami.setAttribute("id", "right");
-    whatami.innerHTML = /* html */ `I am a Front-End Engineer / New Media Artist with a background in art and computer science. I am passionate about crafting immersive and engaging experiences, playing my tiny little part in shaping the future of the web. But, enough about me, tell me about yourself.`;
+    whatami.innerHTML = /* html */ `I am a Front-End Engineer / New Media Artist with a background in art and computer science, always passionate about crafting immersive and engaging experiences, playing my tiny little part in shaping the future of the web.`;
     intro.appendChild(whatami);
   }
 }
