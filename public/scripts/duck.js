824a908fe93d9310b9e64924e676e6f6e6e97bb9
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-02-25 03:00:10 +0200

    date correction

--- public/scripts/duck.js
+++ public/scripts/duck.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Michael Kolesidis (michael.kolesidis@gmail.com)
+// Copyright (c) 2023 Michael Kolesidis (michael.kolesidis@gmail.com)
 // Licensed under the GNU General Public License v3.0.
 // https://www.gnu.org/licenses/gpl-3.0.html
 
