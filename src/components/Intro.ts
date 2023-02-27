bf16513c53671bb5a6d4170f796078b1648e3127
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-02-28 00:40:27 +0200

    dom window manager as package and scroll message positioning

--- src/components/Intro.ts
+++ src/components/Intro.ts
@@ -2,7 +2,7 @@
 // Licensed under the GNU Affero General Public License v3.0.
 // https://www.gnu.org/licenses/gpl-3.0.html
 
-import { WindowManager, dragElement }  from "../utils/windowManager.js";
+import { WindowManager, dragElement } from "dom-window-manager";
 
 let windowManager = new WindowManager();
 
