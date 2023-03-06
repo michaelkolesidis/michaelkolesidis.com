c3aa0184d149f0320a544a0ad18f9eead59b5f5b
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-03-06 14:39:04 +0200

    disable p5.js on mobile

--- public/scripts/duck.js
+++ public/scripts/duck.js
@@ -2,41 +2,41 @@
 // Licensed under the GNU General Public License v3.0.
 // https://www.gnu.org/licenses/gpl-3.0.html
 
-const duck = (sketch) => {
-  let duck;
-  let duckColor;
-  let backgroundColor;
+if (window.innerWidth > 700) {
+  const duck = (sketch) => {
+    let duck;
+    let duckColor;
+    let backgroundColor;
 
-  sketch.preload = () => {
-    // Load model with normalise parameter set to true will affect translate
-    img = sketch.loadImage("assets/3d-models/duck.png");
-    duck = sketch.loadModel("./assets/3d-models/duck.obj", false);
-  };
+    sketch.preload = () => {
+      // Load model with normalise parameter set to true will affect translate
+      img = sketch.loadImage("assets/3d-models/duck.png");
+      duck = sketch.loadModel("./assets/3d-models/duck.obj", false);
+    };
 
-  sketch.setup = () => {
-    cnv = sketch.createCanvas(
-      window.innerWidth,
-      window.innerHeight > 1050
-        ? window.innerHeight - 140
-        : window.innerHeight - 30,
-      sketch.WEBGL
-    );
+    sketch.setup = () => {
+      cnv = sketch.createCanvas(
+        window.innerWidth,
+        window.innerHeight > 1050
+          ? window.innerHeight - 140
+          : window.innerHeight - 30,
+        sketch.WEBGL
+      );
 
-    cnv.mouseClicked(sketch.recolorize);
+      cnv.mouseClicked(sketch.recolorize);
 
-    sketch.colorize(); // random color
-    // duckColor = sketch.color(255, 201, 0); // yellow color
-    // backgroundColor = sketch.color(255, 201, 0);
-  };
+      sketch.colorize(); // random color
+      // duckColor = sketch.color(255, 201, 0); // yellow color
+      // backgroundColor = sketch.color(255, 201, 0);
+    };
 
-  sketch.draw = () => {
-    sketch.background(backgroundColor);
-    sketch.noStroke();
-    sketch.ambientLight(100, 100, 100);
-    sketch.directionalLight(255, 255, 255, 0, 0, -1);
-    sketch.ambientMaterial(duckColor);
+    sketch.draw = () => {
+      sketch.background(backgroundColor);
+      sketch.noStroke();
+      sketch.ambientLight(100, 100, 100);
+      sketch.directionalLight(255, 255, 255, 0, 0, -1);
+      sketch.ambientMaterial(duckColor);
 
-    if (window.innerWidth > 1050) {
       // Desktop
       sketch.camera(
         0.1 * (sketch.mouseX - sketch.windowWidth / 2),
@@ -47,92 +47,86 @@ const duck = (sketch) => {
       sketch.rotateX(sketch.PI - sketch.radians(30));
       sketch.rotateY(sketch.PI + sketch.radians(10));
       sketch.translate(0, -100, -20);
-    } else {
-      // Mobile
-      sketch.rotateX(sketch.PI - sketch.radians(30));
-      sketch.rotateY(sketch.PI + sketch.radians(10));
-      sketch.scale((sketch.windowWidth / 1000) * 4);
-      sketch.translate(0, -150, -20);
-    }
 
-    sketch.texture(img);
-    sketch.scale(210);
-    // sketch.rotateY(sketch.frameCount * 0.01); // auto rotate model
-    sketch.rotateY(0.1);
-    sketch.model(duck);
-  };
+      sketch.texture(img);
+      sketch.scale(210);
+      // sketch.rotateY(sketch.frameCount * 0.01); // auto rotate model
+      sketch.rotateY(0.1);
+      sketch.model(duck);
+    };
 
-  sketch.colorize = () => {
-    let chosenColor = generateColor();
-    duckColor = chosenColor;
-    backgroundColor = chosenColor;
-  };
+    sketch.colorize = () => {
+      let chosenColor = generateColor();
+      duckColor = chosenColor;
+      backgroundColor = chosenColor;
+    };
 
-  sketch.recolorize = () => {
-    let isNew = false;
-    let oldColor = duckColor;
-    let chosenColor;
+    sketch.recolorize = () => {
+      let isNew = false;
+      let oldColor = duckColor;
+      let chosenColor;
 
-    while (isNew === false) {
-      chosenColor = generateColor();
+      while (isNew === false) {
+        chosenColor = generateColor();
 
-      if (
-        oldColor["levels"][0] !== chosenColor["levels"][0] ||
-        oldColor["levels"][1] !== chosenColor["levels"][1] ||
-        oldColor["levels"][2] !== chosenColor["levels"][2]
-      ) {
-        isNew = true;
+        if (
+          oldColor["levels"][0] !== chosenColor["levels"][0] ||
+          oldColor["levels"][1] !== chosenColor["levels"][1] ||
+          oldColor["levels"][2] !== chosenColor["levels"][2]
+        ) {
+          isNew = true;
+        }
       }
-    }
 
-    duckColor = chosenColor;
-    backgroundColor = chosenColor;
+      duckColor = chosenColor;
+      backgroundColor = chosenColor;
+    };
+
+    sketch.windowResized = () => {
+      sketch.resizeCanvas(window.innerWidth, window.innerHeight);
+    };
+
+    generateColor = () => {
+      const colors = [
+        {
+          name: `pink`,
+          hex: `#ff90e8`,
+          rgb: `255, 144, 232`,
+        },
+        {
+          name: `yellow`,
+          hex: `#ffc900`,
+          rgb: `255, 201, 0`,
+        },
+        {
+          name: `orange`,
+          hex: `#ff7051`,
+          rgb: `255, 112, 81`,
+        },
+        {
+          name: `green`,
+          hex: `#3ecfc1`,
+          rgb: `62, 207, 193`,
+        },
+        {
+          name: `purple`,
+          hex: `#90a8ed`,
+          rgb: `144, 168, 237`,
+        },
+      ];
+
+      const colorsP5 = [
+        sketch.color(255, 144, 232),
+        sketch.color(255, 201, 0),
+        sketch.color(255, 112, 81),
+        sketch.color(62, 207, 193),
+        sketch.color(144, 168, 237),
+      ];
+
+      const num = Math.floor(Math.random() * colors.length);
+      return colorsP5[num];
+    };
   };
 
-  sketch.windowResized = () => {
-    sketch.resizeCanvas(window.innerWidth, window.innerHeight);
-  };
-
-  generateColor = () => {
-    const colors = [
-      {
-        name: `pink`,
-        hex: `#ff90e8`,
-        rgb: `255, 144, 232`,
-      },
-      {
-        name: `yellow`,
-        hex: `#ffc900`,
-        rgb: `255, 201, 0`,
-      },
-      {
-        name: `orange`,
-        hex: `#ff7051`,
-        rgb: `255, 112, 81`,
-      },
-      {
-        name: `green`,
-        hex: `#3ecfc1`,
-        rgb: `62, 207, 193`,
-      },
-      {
-        name: `purple`,
-        hex: `#90a8ed`,
-        rgb: `144, 168, 237`,
-      },
-    ];
-
-    const colorsP5 = [
-      sketch.color(255, 144, 232),
-      sketch.color(255, 201, 0),
-      sketch.color(255, 112, 81),
-      sketch.color(62, 207, 193),
-      sketch.color(144, 168, 237),
-    ];
-
-    const num = Math.floor(Math.random() * colors.length);
-    return colorsP5[num];
-  };
-};
-
-const duckSketch = new p5(duck, "sketch-placeholder");
+  const duckSketch = new p5(duck, "sketch-placeholder");
+}
