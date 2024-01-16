00240c47f26c194b866f9e704ed9547a9198ead5
Michael Kolesidis <michael.kolesidis@gmail.com>
2024-01-16 04:01:42 +0000

    empty page

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,8 @@
 
 import "./style/style.scss";
 import body from "./components/Body.js";
-import ticker from "./components/Ticker.js";
 import header from "./components/Header.js";
 import intro from "./components/Intro.js";
-import projects from "./components/Projects.js";
-import contact from "./components/Contact.js";
-import about from "./components/About.js";
-import awards from "./components/Awards.js";
-import thisWebsite from "./components/ThisWebsite.js";
-import footer from "./components/Footer.js";
-import topButton from "./components/TopButton.js";
 import { getScrollDistanceCss } from "./utils/functions.js";
 import { disableRightClick } from "./utils/functions.js";
 
