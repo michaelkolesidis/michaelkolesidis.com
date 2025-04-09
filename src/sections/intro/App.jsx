372218b1c3296fff9f1f67776c24e01e5c165c30
Michael Kolesidis <michael.kolesidis@gmail.com>
2025-04-09 18:20:22 +0300

    duck movement float

--- src/sections/intro/App.jsx
+++ src/sections/intro/App.jsx
@@ -1,23 +1,7 @@
-/*
- *  Copyright (c) Michael Kolesidis <michael.kolesidis@gmail.com>
- *  GNU Affero General Public License v3.0
- *
- *  ATTENTION! FREE SOFTWARE
- *  This website is free software (free as in freedom).
- *  If you use any part of this code, you must make your entire project's source code
- *  publicly available under the same license. This applies whether you modify the code
- *  or use it as it is in your own project. This ensures that all modifications and
- *  derivative works remain free software, so that everyone can benefit.
- *  If you are not willing to comply with these terms, you must refrain from using any part of this code.
- *
- *  For full license terms and conditions, you can read the AGPL-3.0 here:
- *  https://www.gnu.org/licenses/agpl-3.0.html
- */
-
 import { useState, useRef, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
 import { Float } from '@react-three/drei';
 import { Duck } from './Duck';
-import { Canvas } from '@react-three/fiber';
 
 export default function App() {
   const duck = useRef();
@@ -39,23 +23,6 @@ export default function App() {
     console.log('clicked!');
   };
 
-  const [mouseX, setMouseX] = useState(0);
-  const [mouseY, setMouseY] = useState(0);
-
-  const handleMouseMove = (e) => {
-    const { clientX, clientY } = e;
-    setMouseX(clientX);
-    setMouseY(clientY);
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
   useEffect(() => {
     if (hovered) {
       document.body.style.cursor = 'pointer';
@@ -70,28 +37,30 @@ export default function App() {
   return (
     <Canvas
       camera={{
-        // fov: 45,
         near: 0.1,
         far: 50,
-        position: [0, 2.5, 5],
+        position: [2, 3, 4],
       }}
     >
       <ambientLight intensity={3.2} />
-      <pointLight intensity={5} position={[0, , 5]} />
+      <pointLight intensity={5} position={[0, 0, 5]} />
 
-      <Duck
-        ref={duck}
-        position={[0, -1.45, 1]}
-        // rotation={[Math.PI / 7 - 0.3, -Math.PI / 5 - 0.3, 0]}
-        scale={[4, 4, 4]}
-        onClick={handleClick}
-        onPointerOver={handleHover}
-        onPointerOut={handleUnhover}
-        mouseX={mouseX}
-        mouseY={mouseY}
-      />
-
-      <Float rotationIntensity={1} floatIntensity={0.25} />
+      <Float
+        speed={2} // defaults to 1
+        rotationIntensity={1} // defaults to 1
+        floatIntensity={2} // defaults to 1
+        floatingRange={[-0.1, 0.1]} // defaults to [-0.1,0.1]
+      >
+        <Duck
+          ref={duck}
+          // position={[0, 0, 0]}
+          scale={[5, 5, 5]}
+          // rotation={[0, 0, 0]}
+          onClick={handleClick}
+          onPointerOver={handleHover}
+          onPointerOut={handleUnhover}
+        />
+      </Float>
     </Canvas>
   );
 }
--- src/sections/intro/Duck.jsx
+++ src/sections/intro/Duck.jsx
@@ -1,65 +1,33 @@
-/*
- *  Copyright (c) Michael Kolesidis <michael.kolesidis@gmail.com>
- *  GNU Affero General Public License v3.0
- *
- *  ATTENTION! FREE SOFTWARE
- *  This website is free software (free as in freedom).
- *  If you use any part of this code, you must make your entire project's source code
- *  publicly available under the same license. This applies whether you modify the code
- *  or use it as it is in your own project. This ensures that all modifications and
- *  derivative works remain free software, so that everyone can benefit.
- *  If you are not willing to comply with these terms, you must refrain from using any part of this code.
- *
- *  For full license terms and conditions, you can read the AGPL-3.0 here:
- *  https://www.gnu.org/licenses/agpl-3.0.html
- */
-
 import { useRef, forwardRef } from 'react';
-import { useFrame } from '@react-three/fiber';
+// import { useFrame } from '@react-three/fiber';
 import { useGLTF, Outlines } from '@react-three/drei';
+// import * as THREE from 'three';
 
-const lerp = (start, end, alpha) => start + (end - start) * alpha;
+// const lerp = (start, end, alpha) => start + (end - start) * alpha;
 
-export const Duck = forwardRef(({ mouseX, mouseY, ...props }, ref) => {
+export const Duck = forwardRef(({ ...props }, ref) => {
   const { nodes, materials } = useGLTF('/assets/3d-models/duck.glb');
   const duck = useRef();
 
-  // Initial defined rotation
-  const initialRotation = [0.01, Math.PI / 128, 0];
-  const rotationSpeed = 0.05;
-  const maxRotation = Math.PI / 16; // Single variable for max rotation
+  // const rotationSpeendX = 0.2;
+  // const rotationSpeendY = -rotationSpeendX / 2;
 
-  useFrame(() => {
-    if (duck.current) {
-      // Normalize mouse coordinates to the range of -1 to 1
-      const normalizedMouseX = (mouseX / window.innerWidth) * 2 - 1;
-      const normalizedMouseY = (mouseY / window.innerHeight) * 2 - 1;
-
-      // Calculate target rotations based on normalized mouse coordinates
-      const targetRotationY = normalizedMouseX * maxRotation * 2; // Increased influence
-      const targetRotationX = normalizedMouseY * maxRotation;
-
-      duck.current.rotation.y = lerp(
-        duck.current.rotation.y,
-        initialRotation[1] - targetRotationY,
-        rotationSpeed
-      );
-      duck.current.rotation.x = lerp(
-        duck.current.rotation.x,
-        initialRotation[0] - targetRotationX,
-        rotationSpeed
-      );
-    }
-  });
+  // useFrame((state) => {
+    // duck.current.rotation.y = THREE.MathUtils.lerp(
+    //   ref.current.rotation.y,
+    //   (state.pointer.x * Math.PI) / 20,
+    //   rotationSpeendX
+    // );
+    // duck.current.rotation.x = THREE.MathUtils.lerp(
+    //   ref.current.rotation.x,
+    //   (state.pointer.y * Math.PI) / 20,
+    //   rotationSpeendY
+    // );
+  // });
 
   return (
     <group {...props} ref={duck} dispose={null}>
-      <mesh
-        ref={ref}
-        name="duck"
-        geometry={nodes.Node1.geometry}
-        rotation={initialRotation}
-      >
+      <mesh ref={ref} name="duck" geometry={nodes.Node1.geometry}>
         <meshStandardMaterial
           map={materials.Duck.map}
           color={0xffffff}
