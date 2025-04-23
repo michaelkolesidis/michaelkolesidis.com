c3208e4a81550524a58beac9c99093de88d2cbf8
Michael Kolesidis <michael.kolesidis@gmail.com>
2025-04-23 14:50:18 +0300

    footer and favicon updates

--- README.md
+++ README.md
@@ -151,6 +151,6 @@ The code is licensed under the [GNU Affero General Public License v3.0](https://
 
 <a href="https://creativecommons.org/licenses/by-sa/4.0/"><img src="./public/assets/icons/creative-commons.svg" height="80px" /></a>
 
-The graphics, the images, and the cursors, are licensed under the [Creative Commons Attribution-ShareAlike 4.0 International](https://creativecommons.org/licenses/by-sa/4.0/).
+The graphics, images, and cursors, are licensed under the [Creative Commons Attribution-ShareAlike 4.0 International](https://creativecommons.org/licenses/by-sa/4.0/), except for the "K" logo.
 
-🏴‍☠️🏳️‍⚧️🏳️‍🌈✊🏿🏴
+🏳️‍⚧️✊🏿🏳️‍🌈
--- src/sections/footer/Footer.ts
+++ src/sections/footer/Footer.ts
@@ -22,29 +22,22 @@ export default function footer() {
   const footer = document.getElementById('footer');
   if (!footer) return;
 
-  // Duck SVG
-  const duck = document.createElement('div');
-  duck.innerHTML = /* html */ `
-  <svg id="duck" version="1.1" viewBox="0 0 22.723 20.862" xmlns="http://www.w3.org/2000/svg">
-  <path id="duck-path" d="m7.9465 0.022017c-0.22997 0.0264-0.48802 0.078-0.80235 0.16037-1.0232 0.26811-1.9512 0.90317-2.6118 1.7872-0.41157 0.5509-0.6444 1.0172-0.87813 1.7587-0.0978 0.31013-0.0978 0.31013-0.42596 0.46827-0.75593 0.36431-1.6087 0.58113-2.5972 0.66046-0.24501 0.0197-0.46789 0.0452-0.49529 0.0567-0.0892 0.0376-0.1358 0.22889-0.1358 0.55779 0 0.27845 3e-3 0.29631 0.0746 0.44752 0.23999 0.50667 0.8298 0.82484 2.1339 1.1511 0.72517 0.18144 1.1845 0.26301 1.759 0.31248 0.23585 0.0203 0.37657 0.0435 0.43657 0.0721 0.16997 0.0808 0.64382 0.64951 0.79685 0.95635 0.10202 0.20454 0.111 0.37584 0.0319 0.60895-0.10991 0.32387-0.31902 0.57071-1.0308 1.2166-0.50858 0.46158-0.80978 0.78095-1.0838 1.1491-0.44637 0.59986-0.7573 1.3205-0.86177 1.9975-0.0659 0.42681-0.0719 1.3171-0.0111 1.6473 0.43909 2.3858 2.6478 4.4849 5.6299 5.3508 1.1939 0.34663 2.2488 0.48278 3.7255 0.48083 1.8982-3e-3 3.49-0.23505 4.9767-0.72702 2.8614-0.94684 4.7837-2.809 5.6579-5.4807 0.25939-0.79278 0.40257-1.5341 0.46939-2.4306 0.0358-0.47974 0.0143-1.9998-0.0351-2.4833-0.10585-1.0364-0.26204-1.7754-0.45719-2.1631-0.0916-0.18185-0.14441-0.21262-0.33997-0.19782-0.19852 0.015-0.38735 0.0979-1.2581 0.55204-0.72301 0.37712-1.11 0.55906-1.5962 0.75057-1.8076 0.71194-3.9923 1.013-5.4911 0.75681-0.52626-0.09-0.97899-0.27726-1.203-0.49767-0.17564-0.17288-0.21292-0.26035-0.21306-0.50014-1.4e-4 -0.24633 0.0246-0.30157 0.45725-1.0209 0.56696-0.94258 0.7145-1.3086 0.8231-2.0417 0.0303-0.20415 0.0398-0.43301 0.0408-0.97446 1e-3 -0.65937-2e-3 -0.72426-0.053-0.95127-0.20279-0.91087-0.58048-1.6072-1.187-2.1884-0.53217-0.50997-1.1952-0.8638-2.0096-1.0725-0.37301-0.0956-0.71066-0.14093-1.2851-0.17251-0.4874-0.0268-0.70196-0.0262-0.95126 2e-3m-1.7517 2.8021c-0.21019 0.0544-0.41151 0.23415-0.51786 0.46234-0.0546 0.1171-0.0614 0.16009-0.0618 0.38695-3.7e-4 0.23898 4e-3 0.26511 0.0733 0.4107 0.17921 0.37825 0.54871 0.56301 0.83447 0.41723 0.31927-0.1629 0.51767-0.58442 0.45449-0.96567-0.0762-0.45997-0.44811-0.79811-0.78256-0.71155" fill="#ffffff" fill-rule="evenodd" stroke-width=".023202"/>
-  </svg>
-  `;
-  footer.appendChild(duck);
+  // Logo SVG
+  const logo = document.createElement('div');
+  logo.innerHTML = /* html */ `
+  <svg width="512" height="512" id="logo" version="1.1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path id="logo-path" d="m0 0h512v512h-512z" style="fill-rule:evenodd;fill:#fff;paint-order:stroke fill markers"/><path d="m99.063 56.003s56.096 73.603 14.976 134.75c-17.909 22.421-38.442 42.701-58.039 63.642 22.529 29.042 53.089 54.533 66.312 89.787 8.1399 25.434 12.091 66.333-19.058 111.81l108.23-1.9e-4c32.259-5e-5 70.598-43.35 27.612-83.594l-153.15-115.96 142.04-107c33.79-25.454 38.522-70.744 6.8626-87.487-9.4485-4.9966-26.75-5.9535-37.284-5.9535zm203.06-0.0018c17.987 25.089 17.321 48.801 12.585 64.93-2.0344 6.9266-8.8828 16.336-14.194 21.224-50.118 46.128-116.72 77.469-174.22 113.68l162.93 104.97c7.7084 4.9665 19.491 14.427 23.9 22.899 18.633 35.796-12.061 72.293-12.061 72.293h108.33c7.8353 0 23.101-1.2388 29.375-5.7306 23.945-17.143 31.334-77.928-35.391-103.58l-230.33-90.852 226.44-89.169c92.185-35.367 51.528-110.67 18.032-110.67z" style="fill-rule:evenodd;paint-order:stroke fill markers;stroke-width:1.2701"/></svg>`;
+  footer.appendChild(logo);
 
-  // Duck squeak
-  const duckImage = document.getElementById('duck');
-  const duckPath = document.getElementById('duck-path');
+  // Logo squeak
+  const logoImage = document.getElementById('logo');
+  const logoPath = document.getElementById('logo-path');
 
-  if (!duckImage || !duckPath) return;
+  if (!logoImage || !logoPath) return;
 
-  duckImage.addEventListener('click', (event) => {
+  logoImage.addEventListener('click', (event) => {
     event.stopPropagation();
-    const squeak = new Audio('../../assets/sounds/squeak.mp3');
-    squeak.playbackRate = 1.35;
-    squeak.play();
-
-    const hoverColor = generateNewColor(colors, duckPath, 'fill').rgb;
-    duckPath.style.fill = `rgb(${hoverColor})`;
+    const hoverColor = generateNewColor(colors, logoPath, 'fill').rgb;
+    logoPath.style.fill = `rgb(${hoverColor})`;
   });
 
   // Copyright
@@ -71,7 +64,7 @@ export default function footer() {
   // Accessible
   const accessible = document.createElement('div');
   accessible.id = 'accessible';
-  accessible.innerHTML = `<p>For a color-blind-friendly version, <a href="/minimal" target="_blank">click here</a>.</p>`;
+  accessible.innerHTML = `<p>There is an <a href="/minimal" target="_blank">accessible version</a> available.</p>`;
   footer.appendChild(accessible);
 
   // Bottom links
